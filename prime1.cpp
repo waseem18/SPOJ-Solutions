9d012d814ef0a4d92c56baabdc44ae94eeabd358
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-05-24 01:00:16 +0530

    SPOJ CLASSICAL PRIME1

--- prime1.cpp
+++ prime1.cpp
@@ -1,55 +1,46 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<cmath>
+#include<vector>
 using namespace std;
 
-//Finding prime nuners using Sieve of Eratosthenes
-vector<int>spojprime(int l,int h)
-{
-    vector<int>primes;
-    for(int i=l;i<=h;i++)
-    {
-        primes.push_back(i);
-    }
-    //removing multiples of each element
-    for(unsigned int i = 0; i < primes.size(); i++)
-    {
-    	//get the value
-    	int v = primes[i];
+vector<int> primes;
 
-    	if (v!=0) {
-    		//remove all multiples of the value
-    		unsigned int x = i+v;
-    		while(x < primes.size()) {
-    			primes[x]=0;
-    			x = x+v;
-    		}
-    	}
+int main(){
+    bool p[31623];
+    for(int i=0;i<=31623;i++) p[i]=1;
+    for(int i=2;i<=31623;i++){
+        if(p[i]==0) continue;
+        if(p[i]){
+            primes.push_back(i);
+            for(int j=2*i;j<=31623;j+=i){
+                p[j]=0;
+            }
+        }
     }
-    return primes;
-}
-
-int main()
-{
-    int testcases;
-    cin>>testcases;
-    int low,high;
-    vector<int> primes;
-    for(int k=0;k<testcases;k++)
-    {
-        cin>>low>>high;
-        if(low==1)
-            low=2;
-        primes = spojprime(low,high);
-
+    int t;
+    long long m,n;
+    cin>>t;
+    int A[100001];
+    for(int i=0; i<t;i++){
+        cin>>m>>n;
+        for(int j=0; j<=n-m;j++) A[j]=1;
+        for(int k=0; primes[k]<=sqrt(n) && k<primes.size() ; k++){
+            long long J;
+            if(m<=primes[k]) J=2*primes[k];
+            else{
+                if(m%primes[k]==0) J=m;
+                else J = (m/primes[k]+1)*primes[k];
+            } 
+            for(int j=J-m;j<=n-m;j+=primes[k]){
+                A[j]=0;
+            }
+        }
+        for(int j=0; j<=n-m; j++){
+            if(A[j]==1 && m+j!=1) {
+                cout<<m+j<<endl;
+            }
+        }
+        cout<<endl;
     }
-
-    //vector<int> primes = spojprime(low,high);
-    for(unsigned int j=0;j<primes.size();j++)
-    {
-        if(primes[j]!=0)
-            cout<<primes[j]<<endl;
-    }
-    cout<<endl;
-    cout<<endl;
     return 0;
 }
