a5c4c80772035b1f45aff1becd9bb5f1430f9f11
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-05-24 17:37:14 +0530

    SPOJ CLASSICAL ACPC10A

---
+++ acpc10a.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    while(a||b||c)
+    {
+        if(b-a==c-b)
+        {
+            printf("AP %d\n",c+(c-b));
+        }
+        else if((b/a)==(c/b))
+        {
+            printf("GP %d\n",c*(c/b));
+        }
+        scanf("%d%d%d",&a,&b,&c);
+    }
+    return 0;
+}
+
+
