aed39461de110ca1d3865d7ff4412f4c137b5b50
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-05-25 12:09:17 +0530

    UVA IRREDUCIBLE BASIC FRACTIONS. TLE

---
+++ irreducibleuva.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include<algorithm>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    while(b)
+    {
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
+
+int main()
+{
+    int input;
+    //cin>>input;
+    scanf("%d",&input);
+    while(input!=0)
+    {
+        int cnt=0;
+        for(int i=1;i<input;i++)
+        {
+            if(gcd(i,input)==1)
+                cnt+=1;
+        }
+        //cout<<cnt<<endl;
+        printf("%d",cnt);
+        printf("\n");
+        scanf("%d",&input);
+    }
+    return 0;
+}
