783a4fbcca34ba22e9b149062bcdef9f18859993
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-05-29 12:30:18 +0530

    MODULAR INVERSE - EXTENDED EUCLEDIAN THEOREM

---
+++ modularinverse.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int gcd (int a, int b, int & x, int & y) {
+	if (a == 0) {
+		x = 0; y = 1;
+		return b;
+	}
+	int x1, y1;
+	int d = gcd (b%a, a, x1, y1);
+	x = y1 - (b / a) * x1;
+	y = x1;
+	return x;
+}
+
+int main()
+{
+    int p,q,r,s;
+    cin>>p>>q;
+    int t = gcd(p,q,r,s);
+    cout<<t<<endl;
+    return 0;
+}
