478502a1a126045f727af7b11fd687eb1e7967b5
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-08-27 09:27:04 +0530

    gcd2

--- gcd2.cpp
+++ gcd2.cpp
@@ -1,16 +1,11 @@
-//Eulers Algorithm
-
 #include<iostream>
 #include<cstdio>
 #include<cstring>
 using namespace std;
-
-// Euclid's Algo
 int gcd(int a, int b){
 if(b==0) return a;
 else return gcd(b,a%b);
 }
-
 int main(){
 int t;
 cin>>t;
@@ -25,11 +20,8 @@ continue;
 int len=strlen(b);
 for(i=0,num=0;i<len;i++){
 num=(num*10+(b[i]-'0'))%a;
-//cout<<num<<" ";
 }
-/*
-Use the property that gcd(a,b) = gcd(b,a%b)
-*/
+
 printf("%d\n",gcd(a,num));
 }
 return 0;
